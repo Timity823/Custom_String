22862a2ddeff19b866cb4f6201220192d1091fe8
Timi <ksngk@gmail.com>
2024-04-06 07:56:36 +0200

    Upload Custom String

---
+++ src/main/java/com/example/custom_string/CustomStringApplication.java
@@ -0,0 +1,13 @@
+package com.example.custom_string;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomStringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomStringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/custom_string/CustomStringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.custom_string;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomStringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
